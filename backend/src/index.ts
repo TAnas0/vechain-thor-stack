7157a63b0355d68b0bc4a5f1194cdb9ee97c344d
Anas <anastiour@gmail.com>
2023-01-02 13:15:04 +0100

    WIP(backend): add TODO comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,18 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
+// TODO Middleware for "authentication"
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server working");
 });
 
+app.get("/status", async (req: Request, res: Response) => {
+  // TODO Get best/finalized block details
+  // TODO Ping other testnet servers and get their details
+  // TODO determine if synced or not
+  // TODO Estimate how long till sync
+});
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
