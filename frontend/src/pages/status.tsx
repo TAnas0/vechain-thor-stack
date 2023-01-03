9a8866c75a530769742c193785fecd57fa58441d
Anas <anastiour@gmail.com>
2023-01-03 20:21:56 +0100

    feat(frontend): display raw status response from backend

---
+++ frontend/src/pages/status.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Main } from "@/templates/Main";
+
+const getNodeStatus = async () => {
+  const resp = await fetch(`http://${process.env.NEXT_PUBLIC_THOR_ENDPOINT}:${process.env.NEXT_PUBLIC_THOR_PORT}/status`);
+  return resp.json();
+};
+
+const Status = () => {
+  const [status, setStatus] = useState({});
+
+  useEffect(() => {
+    getNodeStatus()
+      .then((resp) => {
+        setStatus(resp);
+        console.log(resp);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <Main meta="">
+      <div>{JSON.stringify(status)}</div>
+    </Main>
+  );
+};
+
+export default Status;
